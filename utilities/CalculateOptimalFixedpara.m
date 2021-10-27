ff44ec6aa722d2b2ae428b1d32c51ae5be97aa65
Tao Hong <taoh1990@gmail.com>
2021-10-27 22:03:44 +0300

    Update CalculateOptimalFixedpara.m

--- utilities/CalculateOptimalFixedpara.m
+++ utilities/CalculateOptimalFixedpara.m
@@ -25,21 +25,6 @@ if abs(b_1)<=b_N
         c_opt = 0;
         r_star = b_N;
         r_star = real(r_star);
-        %     else
-        %         b_N_new = -b_1;
-        %         b_1_new = -b_N;
-        %         if b_1_new>=-1/3*b_N_new
-        %             c_opt = (2-2*sqrt(1-b_N_new))/b_N_new-1;
-        %             r_star = 1-sqrt(1-b_N_new);
-        %         elseif b_1_new>-b_N_new && b_1_new<0
-        %             Omega = 2*b_1_new*b_N_new*(b_1_new+b_N_new);
-        %             c_opt = (sqrt((4*Omega+(b_1_new-b_N_new)^2)*(b_1_new-b_N_new)^2)-(2*Omega+(b_1_new-b_N_new)^2))/(2*Omega);
-        %             r_star = 0.5*((1+c_opt)*b_N_new+sqrt((1+c_opt)^2*b_N_new^2-4*c_opt*b_N_new));
-        %         elseif b_1_new == -b_N_new
-        %             c_opt = 0;
-        %             r_star = b_N_new;
-        %         end
-        %         r_star = real(r_star);
     else
         error('Not define for such values of b\n')
     end
