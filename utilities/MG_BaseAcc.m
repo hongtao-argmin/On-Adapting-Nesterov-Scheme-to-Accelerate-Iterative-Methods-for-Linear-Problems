6b13668423e3a8aa9ed0a9cf7fd08cb301964ba6
Tao Hong <taoh1990@gmail.com>
2021-10-27 22:05:33 +0300

    Update MG_BaseAcc.m

--- utilities/MG_BaseAcc.m
+++ utilities/MG_BaseAcc.m
@@ -13,15 +13,7 @@ CPUTime = zeros(n_cycles+1,1);
 for iters = 1:n_cycles
     StartTime = tic;
     u_k_1 = V_cycle(y_k,f,h,nu1,nu2,w,L_stencil,bndcond,CycType,RelaxationMethod);
-    %     if mod(iters,100)==0
-    %        y_k = u_k_1;
-    %     else
-    %t_k_1 = (1+sqrt(1+4*t_k^2))/2;
-    %y_k = u_k_1 + (iters-2)/(iters+1)*(u_k_1-u_k);%(t_k-1)/t_k_1
-    %t_k = t_k_1;
     y_k = u_k_1 + accpara*(u_k_1-u_k);
-    
-    %     end
     u_k = u_k_1;
     CPUTime(iters+1) = toc(StartTime);
     r = f - Lu(u_k,L_stencil,h,bndcond);
@@ -37,16 +29,5 @@ CPUTime = cumsum(CPUTime);
 CPUTime = CPUTime(1:n_cycles+1);
 temp = rnorm(2:end) ./ rnorm(1:end-1);
 fprintf('%f\n',geomean(temp(end-5:end)))
-% figure
-% semilogy([0:n_cycles],rnorm,'k-');
-% title('Residual Norm History')
-% xlabel('Iterations')
-% ylabel('Residual Norm')
-% % Plot residual norm convergence factors.
-% figure
-% plot([1:n_cycles],rnorm(2:end) ./ rnorm(1:end-1),'k-');
-% title('Residual Convergence Factor Per Cycle')
-% xlabel('Iterations')
-% ylabel('Convergence Factor')
 
 end
